7fd862e40d21419383367515d6f019282b758bc1
rahulnayi04 <you@example.com>
2023-10-05 00:04:08 +0530

    added authentication Auth0

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+
 import Home from './Components/Home'
 
 function App() {
@@ -7,7 +8,9 @@ function App() {
 
   return (
     <>
-      <Home />
+    <Home />
+    
+      
     </>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import './Navbar.css'; // Import the CSS file for styling
 
 const Navbar = () => {
+  const {user, loginWithRedirect, isAuthenticated, logout } = useAuth0();
   return (
     <nav className="navbar">
       <div className="logo">
@@ -15,7 +17,8 @@ const Navbar = () => {
         <li><a href="#contact">Contact</a></li>
       </ul>
       <div className="login-button">
-        <button>Login</button>
+        {isAuthenticated ? <button onClick={logout}>Log out</button> : <button onClick={(e)=> loginWithRedirect()}>Login</button>}
+        
       </div>
     </nav>
   );
